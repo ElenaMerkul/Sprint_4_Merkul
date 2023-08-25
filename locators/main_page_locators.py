b94dd94a734b4b2bf2963d6d8fcf433f4373d060
Elena Merkul <merkul-e@mail.ru>
2023-08-25 18:04:04 +0300

    Sprint_4

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -6,7 +6,9 @@ class MainPageLocators:
     LOCATOR_FAQ = By.XPATH, "//div[contains(@class, 'Home_FAQ_')]"  # Раздел "Вопросы о важном"
     LOCATOR_QUESTIONS = By.XPATH, "//div[contains(@class, 'accordion__item')]"  # Вопросы
     LOCATOR_RESPONSE = By.XPATH, "//div[contains(@class, 'accordion__panel') and not(@hidden)]"  # Отображаемый ответ
-    COOKIES_BUTTON = By.XPATH, "//button[contains(@class, 'App_CookieButton_')]"  # Кнопка принять куки
+    COOKIES_BUTTON = By.ID, "rcc-confirm-button"  # Кнопка принять куки
     HEADER_TEXT = By.XPATH, "//div[contains(@class, 'Home_Header')]"  # Заголовок на главной странице
-    HEADER_ORDER_BUTTON = By.XPATH, "//*[contains(@class, 'Header_Nav')]/button[text()='Заказать']" # Кнопка "Заказать" в шапке
-    ORDER_BUTTON_MAIN_PAGE = By.XPATH, "//div[contains(@class, 'Home_FinishButton')]/button[contains(@class, 'Button_Button__ra12g')]" # Кнопка "Заказать" на странице
+    HEADER_ORDER_BUTTON = By.XPATH, "//div[contains(@class, 'Header')]//button[text()='Заказать']"  # Кнопка "Заказать" в шапке
+    # либо HEADER_ORDER_BUTTON = By.XPATH, "//button[text()='Заказать'][1]"
+    ORDER_BUTTON_MAIN_PAGE = By.XPATH, "//div[contains(@class, 'Finish')]//button[text()='Заказать']"  # Кнопка "Заказать" на странице
+    # либо ORDER_BUTTON_MAIN_PAGE = By.XPATH, "//button[text()='Заказать'][2]"
